6ffe15091811c1ae89830121acfa176e0348f27f
Salvo-02 <salvo.dicesare02@gmail.com>
2025-10-01 14:14:31 +0200

    fine

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -2,8 +2,8 @@ import Alert from "react-bootstrap/Alert";
 
 const FirstAlert = function () {
   return (
-    <Alert variant="dark">
-      <Alert.Heading>Welcome</Alert.Heading>
+    <Alert variant="dark text-center">
+      <Alert.Heading className="display-3">Welcome</Alert.Heading>
       <p>Benvenuto su CineShop! Scopri i migliori film in DVD, Blu-ray e digital download. Buona visione!</p>
       <hr />
       <p className="mb-0">Dove ogni acquisto è da Oscar… o almeno da pop-corn! 🍿</p>
